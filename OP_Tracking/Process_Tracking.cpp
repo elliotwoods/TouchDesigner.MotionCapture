0dd16aa42b48a5d3809cd1317fdde6b1fe83c231
Elliot Woods <elliot@kimchiandchips.com>
2020-08-29 18:53:37 +0900

    Fixes #19

--- OP_Tracking/Process_Tracking.cpp
+++ OP_Tracking/Process_Tracking.cpp
@@ -85,260 +85,267 @@ namespace TD_MoCap {
 			, std::shared_ptr<Frames::TrackingFrame> outputFrame
 			, TrackingParameters& parameters)
 	{
-		auto leftID = inputFrame->inputFrame->inputFrame->leaderID;
-		auto rightID = inputFrame->inputFrame->inputFrame->secondaryID;
-
-		const auto& currentCentroidsLeft = inputFrame->cameraLeftCentroids;
-		const auto& currentCentroidsRight = inputFrame->cameraRightCentroids;
+		// if we have a previous frame
 		if (this->previousFrame) {
-			//calculate where current frame centroids might be in previous frames
-			std::vector<cv::Point2f> backupCentroidsLeft;
-			std::vector<cv::Point2f> backupCentroidsRight;
 			
-			const auto opticalFlowMethod = parameters.opticalFlowMethod.getSelectedOption();
-			if(opticalFlowMethod == "CPU") {
-				std::vector<std::function<void()>> actions;
-				auto windowSize = parameters.opticalFlowRadius.getValue() * 2 + 1;
+			// if the frames are sequential (e.g. no dropped frame)
+			if (inputFrame->getFrameIndex() == this->previousFrame->getFrameIndex() + 1) {
 
-				actions.emplace_back([&] {
-					const auto& priorImage = this->previousFrame->inputFrame->inputFrame->cameras[leftID]->greyscale;
-					const auto& currentImage = inputFrame->inputFrame->cameras[leftID]->greyscale;
-					std::vector<uchar> status;
-					std::vector<float> error;
-					cv::calcOpticalFlowPyrLK(currentImage
-						, priorImage
-						, currentCentroidsLeft
-						, backupCentroidsLeft
-						, status
-						, error
-						, cv::Size(windowSize, windowSize)
-					);
-				});
+				auto leftID = inputFrame->inputFrame->inputFrame->leaderID;
+				auto rightID = inputFrame->inputFrame->inputFrame->secondaryID;
 
-				actions.emplace_back([&] {
-					const auto& priorImage = this->previousFrame->inputFrame->inputFrame->cameras[rightID]->greyscale;
-					const auto& currentImage = inputFrame->inputFrame->cameras[rightID]->greyscale;
-					std::vector<uchar> status;
-					std::vector<float> error;
-					cv::calcOpticalFlowPyrLK(currentImage
-						, priorImage
-						, currentCentroidsRight
-						, backupCentroidsRight
-						, status
-						, error
-						, cv::Size(windowSize, windowSize)
-					);
-				});
+				const auto& currentCentroidsLeft = inputFrame->cameraLeftCentroids;
+				const auto& currentCentroidsRight = inputFrame->cameraRightCentroids;
 
-				// perform the search
-				Utils::WorkerGroup::X().parallelFor(actions);
-			}
-			else if (opticalFlowMethod == "CUDA sparse") {
-				auto windowSize = parameters.opticalFlowRadius.getValue() * 2 + 1;
-				static auto opticalFlow = cv::cuda::SparsePyrLKOpticalFlow::create(cv::Size(windowSize, windowSize));
+				//calculate where current frame centroids might be in previous frames
+				std::vector<cv::Point2f> backupCentroidsLeft;
+				std::vector<cv::Point2f> backupCentroidsRight;
 
-				cv::cuda::Stream actionQueue;
-				cv::cuda::GpuMat backupCentroidsLeftGPU, backupCentroidsRightGPU;
-				{
-					const auto& priorImage = cv::cuda::GpuMat(this->previousFrame->inputFrame->inputFrame->cameras[leftID]->greyscale);
-					const auto& currentImage = cv::cuda::GpuMat(inputFrame->inputFrame->cameras[leftID]->greyscale);
-					auto status = cv::cuda::GpuMat();
-					auto error= cv::cuda::GpuMat();
+				const auto opticalFlowMethod = parameters.opticalFlowMethod.getSelectedOption();
+				if (opticalFlowMethod == "CPU") {
+					std::vector<std::function<void()>> actions;
+					auto windowSize = parameters.opticalFlowRadius.getValue() * 2 + 1;
 
+					actions.emplace_back([&] {
+						const auto& priorImage = this->previousFrame->inputFrame->inputFrame->cameras[leftID]->greyscale;
+						const auto& currentImage = inputFrame->inputFrame->cameras[leftID]->greyscale;
+						std::vector<uchar> status;
+						std::vector<float> error;
+						cv::calcOpticalFlowPyrLK(currentImage
+							, priorImage
+							, currentCentroidsLeft
+							, backupCentroidsLeft
+							, status
+							, error
+							, cv::Size(windowSize, windowSize)
+						);
+					});
 
-					opticalFlow->calc(currentImage
-						, priorImage
-						, cv::cuda::GpuMat(currentCentroidsLeft)
-						, backupCentroidsLeftGPU
-						, status
-						, error
-						, actionQueue
-					);
+					actions.emplace_back([&] {
+						const auto& priorImage = this->previousFrame->inputFrame->inputFrame->cameras[rightID]->greyscale;
+						const auto& currentImage = inputFrame->inputFrame->cameras[rightID]->greyscale;
+						std::vector<uchar> status;
+						std::vector<float> error;
+						cv::calcOpticalFlowPyrLK(currentImage
+							, priorImage
+							, currentCentroidsRight
+							, backupCentroidsRight
+							, status
+							, error
+							, cv::Size(windowSize, windowSize)
+						);
+					});
+
+					// perform the search
+					Utils::WorkerGroup::X().parallelFor(actions);
 				}
+				else if (opticalFlowMethod == "CUDA sparse") {
+					auto windowSize = parameters.opticalFlowRadius.getValue() * 2 + 1;
+					static auto opticalFlow = cv::cuda::SparsePyrLKOpticalFlow::create(cv::Size(windowSize, windowSize));
 
-				{
-					const auto& priorImage = cv::cuda::GpuMat(this->previousFrame->inputFrame->inputFrame->cameras[rightID]->greyscale);
-					const auto& currentImage = cv::cuda::GpuMat(inputFrame->inputFrame->cameras[rightID]->greyscale);
-					auto status = cv::cuda::GpuMat();
-					auto error = cv::cuda::GpuMat();
-
-					opticalFlow->calc(currentImage
-						, priorImage
-						, cv::cuda::GpuMat(currentCentroidsRight)
-						, backupCentroidsRightGPU
-						, status
-						, error
-						, actionQueue
-					);
-				}
-
-				actionQueue.waitForCompletion();
-				backupCentroidsLeftGPU.download(backupCentroidsLeft);
-				backupCentroidsRightGPU.download(backupCentroidsRight);
-			}
-			else if (opticalFlowMethod == "CUDA dense async") {
-				auto beginDownloadTime = std::chrono::high_resolution_clock::now();
-
-				// Download the result and wait
-				auto& opticalFlow = inputFrame->inputFrame->inputFrame->opticalFlow;
-
-				// wait for results
-				for (const auto& it: opticalFlow.results) {
-					it.second->lockThreadJoin.lock();
-					it.second->thread.join();
-					it.second->lockThreadJoin.unlock();
-				}
-
-				auto& flowLeft = opticalFlow.results[leftID]->denseFlowCPU;
-				auto& flowRight = opticalFlow.results[rightID]->denseFlowCPU;
-
-				auto endDownloadTime = std::chrono::high_resolution_clock::now();
-				auto downloadDuration = endDownloadTime - beginDownloadTime;
-				std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(downloadDuration).count() << "ms download time" << std::endl;
-
-				auto getValueBilinear = [](const cv::Mat& img, cv::Point2f pt)
-				{
-					const int x = (int)pt.x;
-					const int y = (int)pt.y;
-
-					int x0 = cv::borderInterpolate(x, img.cols, cv::BORDER_REFLECT_101);
-					int x1 = cv::borderInterpolate(x + 1, img.cols, cv::BORDER_REFLECT_101);
-					int y0 = cv::borderInterpolate(y, img.rows, cv::BORDER_REFLECT_101);
-					int y1 = cv::borderInterpolate(y + 1, img.rows, cv::BORDER_REFLECT_101);
-
-					float a = pt.x - (float)x;
-					float c = pt.y - (float)y;
-
-					const auto& upLeft = img.at<cv::Vec2f>(y0, x0);
-					const auto& upRight = img.at<cv::Vec2f>(y0, x1);
-					const auto& downLeft = img.at<cv::Vec2f>(y1, x0);
-					const auto& downRight = img.at<cv::Vec2f>(y1, x1);
-
-					return (upLeft * (1.0f - a) + upRight * a) * (1.0f - c)
-						+ (downLeft * (1.0f - a) + downRight * a) * c;
-				};
-
-				// 'pipet' the results
-				{
-					// Left
+					cv::cuda::Stream actionQueue;
+					cv::cuda::GpuMat backupCentroidsLeftGPU, backupCentroidsRightGPU;
 					{
-						for (const auto& currentCentroid : currentCentroidsLeft)
+						const auto& priorImage = cv::cuda::GpuMat(this->previousFrame->inputFrame->inputFrame->cameras[leftID]->greyscale);
+						const auto& currentImage = cv::cuda::GpuMat(inputFrame->inputFrame->cameras[leftID]->greyscale);
+						auto status = cv::cuda::GpuMat();
+						auto error = cv::cuda::GpuMat();
+
+
+						opticalFlow->calc(currentImage
+							, priorImage
+							, cv::cuda::GpuMat(currentCentroidsLeft)
+							, backupCentroidsLeftGPU
+							, status
+							, error
+							, actionQueue
+						);
+					}
+
+					{
+						const auto& priorImage = cv::cuda::GpuMat(this->previousFrame->inputFrame->inputFrame->cameras[rightID]->greyscale);
+						const auto& currentImage = cv::cuda::GpuMat(inputFrame->inputFrame->cameras[rightID]->greyscale);
+						auto status = cv::cuda::GpuMat();
+						auto error = cv::cuda::GpuMat();
+
+						opticalFlow->calc(currentImage
+							, priorImage
+							, cv::cuda::GpuMat(currentCentroidsRight)
+							, backupCentroidsRightGPU
+							, status
+							, error
+							, actionQueue
+						);
+					}
+
+					backupCentroidsLeftGPU.download(backupCentroidsLeft, actionQueue);
+					backupCentroidsRightGPU.download(backupCentroidsRight, actionQueue);
+					actionQueue.waitForCompletion();
+				}
+				else if (opticalFlowMethod == "CUDA dense async") {
+					auto beginDownloadTime = std::chrono::high_resolution_clock::now();
+
+					// Download the result and wait
+					auto& opticalFlow = inputFrame->inputFrame->inputFrame->opticalFlow;
+
+					// wait for results
+					for (const auto& it : opticalFlow.results) {
+						it.second->lockThreadJoin.lock();
+						it.second->thread.join();
+						it.second->lockThreadJoin.unlock();
+					}
+
+					auto& flowLeft = opticalFlow.results[leftID]->denseFlowCPU;
+					auto& flowRight = opticalFlow.results[rightID]->denseFlowCPU;
+
+					auto endDownloadTime = std::chrono::high_resolution_clock::now();
+					auto downloadDuration = endDownloadTime - beginDownloadTime;
+					std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(downloadDuration).count() << "ms download time" << std::endl;
+
+					auto getValueBilinear = [](const cv::Mat& img, cv::Point2f pt)
+					{
+						const int x = (int)pt.x;
+						const int y = (int)pt.y;
+
+						int x0 = cv::borderInterpolate(x, img.cols, cv::BORDER_REFLECT_101);
+						int x1 = cv::borderInterpolate(x + 1, img.cols, cv::BORDER_REFLECT_101);
+						int y0 = cv::borderInterpolate(y, img.rows, cv::BORDER_REFLECT_101);
+						int y1 = cv::borderInterpolate(y + 1, img.rows, cv::BORDER_REFLECT_101);
+
+						float a = pt.x - (float)x;
+						float c = pt.y - (float)y;
+
+						const auto& upLeft = img.at<cv::Vec2f>(y0, x0);
+						const auto& upRight = img.at<cv::Vec2f>(y0, x1);
+						const auto& downLeft = img.at<cv::Vec2f>(y1, x0);
+						const auto& downRight = img.at<cv::Vec2f>(y1, x1);
+
+						return (upLeft * (1.0f - a) + upRight * a) * (1.0f - c)
+							+ (downLeft * (1.0f - a) + downRight * a) * c;
+					};
+
+					// 'pipet' the results
+					{
+						// Left
 						{
-							auto flowVector = getValueBilinear(flowLeft, currentCentroid);
-							auto backupCentroid = currentCentroid;
-							backupCentroid.x += flowVector[0];
-							backupCentroid.y += flowVector[1];
-							backupCentroidsLeft.push_back(backupCentroid);
+							for (const auto& currentCentroid : currentCentroidsLeft)
+							{
+								auto flowVector = getValueBilinear(flowLeft, currentCentroid);
+								auto backupCentroid = currentCentroid;
+								backupCentroid.x += flowVector[0];
+								backupCentroid.y += flowVector[1];
+								backupCentroidsLeft.push_back(backupCentroid);
+							}
+						}
+						// Right
+						{
+							for (const auto& currentCentroid : currentCentroidsRight)
+							{
+								auto flowVector = getValueBilinear(flowRight, currentCentroid);
+								auto backupCentroid = currentCentroid;
+								backupCentroid.x += flowVector[0];
+								backupCentroid.y += flowVector[1];
+								backupCentroidsRight.push_back(backupCentroid);
+							}
 						}
 					}
-					// Right
-					{
-						for (const auto& currentCentroid : currentCentroidsRight)
-						{
-							auto flowVector = getValueBilinear(flowRight, currentCentroid);
-							auto backupCentroid = currentCentroid;
-							backupCentroid.x += flowVector[0];
-							backupCentroid.y += flowVector[1];
-							backupCentroidsRight.push_back(backupCentroid);
-						}
-					}
 				}
-			}
 
 
-			//search for correspondences between cameras in QT Tree
-			const auto maxDistance = parameters.searchRadius.getValue();
-			if (parameters.useQuadTree.getValue()) {
-				// sort the previous centroids, we'll search through them when looking through backup centroids
-				const auto previousCentroidsLeftQT = imagePointsToQT(previousFrame->inputFrame->cameraLeftCentroids);
-				const auto previousCentroidsRightQT = imagePointsToQT(previousFrame->inputFrame->cameraRightCentroids);
+				//search for correspondences between cameras in QT Tree
+				const auto maxDistance = parameters.searchRadius.getValue();
+				if (parameters.useQuadTree.getValue()) {
+					// sort the previous centroids, we'll search through them when looking through backup centroids
+					const auto previousCentroidsLeftQT = imagePointsToQT(previousFrame->inputFrame->cameraLeftCentroids);
+					const auto previousCentroidsRightQT = imagePointsToQT(previousFrame->inputFrame->cameraRightCentroids);
 
 
-				// iterate over back-inferred centroids
-				for (size_t i = 0; i < backupCentroidsLeft.size(); i++)
-				{
-					// find the centroid (and therefore triangulated point) indices which match the backed-up points
-					auto findsLeft = findAdjacentQTPoints(previousCentroidsLeftQT
-						, backupCentroidsLeft[i]
-						, maxDistance);
-					auto findsRight = findAdjacentQTPoints(previousCentroidsRightQT
-						, backupCentroidsRight[i]
-						, maxDistance);
+					// iterate over back-inferred centroids
+					for (size_t i = 0; i < backupCentroidsLeft.size(); i++)
+					{
+						// find the centroid (and therefore triangulated point) indices which match the backed-up points
+						auto findsLeft = findAdjacentQTPoints(previousCentroidsLeftQT
+							, backupCentroidsLeft[i]
+							, maxDistance);
+						auto findsRight = findAdjacentQTPoints(previousCentroidsRightQT
+							, backupCentroidsRight[i]
+							, maxDistance);
 
-					// look for matching indices
-					for (const auto& findLeft : findsLeft) {
-						for (const auto& findRight : findsRight) {
-							if (findLeft == findRight) {
-								// was a match in the previous frame
-								auto findPrevious = this->previousFrame->trackedParticles.find(findLeft);
-								if (findPrevious != this->previousFrame->trackedParticles.end()) {
-									auto continuingParticle = findPrevious->second; // copy the particle
-									continuingParticle.lifeTime++;
-									continuingParticle.priorTriangulatedParticleIndex = findPrevious->first;
-									continuingParticle.priorTriangulatedParticlePosition = this->previousFrame->inputFrame->worldPoints[findPrevious->first];
+						// look for matching indices
+						for (const auto& findLeft : findsLeft) {
+							for (const auto& findRight : findsRight) {
+								if (findLeft == findRight) {
+									// was a match in the previous frame
+									auto findPrevious = this->previousFrame->trackedParticles.find(findLeft);
+									if (findPrevious != this->previousFrame->trackedParticles.end()) {
+										auto continuingParticle = findPrevious->second; // copy the particle
+										continuingParticle.lifeTime++;
+										continuingParticle.priorTriangulatedParticleIndex = findPrevious->first;
+										continuingParticle.priorTriangulatedParticlePosition = this->previousFrame->inputFrame->worldPoints[findPrevious->first];
 
-									outputFrame->trackedParticles.emplace(i, continuingParticle);
-								}
-								else {
-									outputFrame->trackedParticles.emplace(i, Frames::TrackingFrame::Particle{
-										 findLeft
-										 , glm::vec3()
-										 , 1
-										});
+										outputFrame->trackedParticles.emplace(i, continuingParticle);
+									}
+									else {
+										outputFrame->trackedParticles.emplace(i, Frames::TrackingFrame::Particle{
+											 findLeft
+											 , glm::vec3()
+											 , 1
+											});
+									}
 								}
 							}
 						}
 					}
 				}
-			}
 
-			// Search without QT Tree
-			else {
-				auto findAdjacentPoints = [&](const std::vector<cv::Point2f>& points
-					, const cv::Point2f& point
-					, float maxDistance)
-				{
-					auto maxDistance2 = maxDistance * maxDistance;
+				// Search without QT Tree
+				else {
+					auto findAdjacentPoints = [&](const std::vector<cv::Point2f>& points
+						, const cv::Point2f& point
+						, float maxDistance)
+					{
+						auto maxDistance2 = maxDistance * maxDistance;
 
-					std::vector<size_t> finds;
-					for (size_t i = 0; i < points.size(); i++) {
-						const auto delta = (points[i] - point);
-						if (delta.dot(delta) <= maxDistance2) {
-							finds.push_back(i);
+						std::vector<size_t> finds;
+						for (size_t i = 0; i < points.size(); i++) {
+							const auto delta = (points[i] - point);
+							if (delta.dot(delta) <= maxDistance2) {
+								finds.push_back(i);
+							}
 						}
-					}
-					return finds;
-				};
+						return finds;
+					};
 
-				for (size_t i = 0; i < backupCentroidsLeft.size(); i++)
-				{
-					auto findsLeft = findAdjacentPoints(previousFrame->inputFrame->cameraLeftCentroids
-						, backupCentroidsLeft[i]
-						, maxDistance);
-					auto findsRight = findAdjacentPoints(previousFrame->inputFrame->cameraRightCentroids
-						, backupCentroidsRight[i]
-						, maxDistance);
+					for (size_t i = 0; i < backupCentroidsLeft.size(); i++)
+					{
+						auto findsLeft = findAdjacentPoints(previousFrame->inputFrame->cameraLeftCentroids
+							, backupCentroidsLeft[i]
+							, maxDistance);
+						auto findsRight = findAdjacentPoints(previousFrame->inputFrame->cameraRightCentroids
+							, backupCentroidsRight[i]
+							, maxDistance);
 
-					// look for matching indices
-					for (const auto& findLeft : findsLeft) {
-						for (const auto& findRight : findsRight) {
-							if (findLeft == findRight) {
-								auto findPrevious = this->previousFrame->trackedParticles.find(findLeft);
-								if (findPrevious != this->previousFrame->trackedParticles.end()) {
-									// was a match in the previous frame
-									auto continuingParticle = findPrevious->second; // copy the particle
-									continuingParticle.lifeTime++;
-									continuingParticle.priorTriangulatedParticleIndex = findPrevious->first;
-									continuingParticle.priorTriangulatedParticlePosition = this->previousFrame->inputFrame->worldPoints[findPrevious->first];
-									outputFrame->trackedParticles.emplace(i, continuingParticle);
-								}
-								else {
-									// new find to add
-									outputFrame->trackedParticles.emplace(i, Frames::TrackingFrame::Particle{
-										 findLeft
-										 , glm::vec3()
-										 , 1
-										});
+						// look for matching indices
+						for (const auto& findLeft : findsLeft) {
+							for (const auto& findRight : findsRight) {
+								if (findLeft == findRight) {
+									auto findPrevious = this->previousFrame->trackedParticles.find(findLeft);
+									if (findPrevious != this->previousFrame->trackedParticles.end()) {
+										// was a match in the previous frame
+										auto continuingParticle = findPrevious->second; // copy the particle
+										continuingParticle.lifeTime++;
+										continuingParticle.priorTriangulatedParticleIndex = findPrevious->first;
+										continuingParticle.priorTriangulatedParticlePosition = this->previousFrame->inputFrame->worldPoints[findPrevious->first];
+										outputFrame->trackedParticles.emplace(i, continuingParticle);
+									}
+									else {
+										// new find to add
+										outputFrame->trackedParticles.emplace(i, Frames::TrackingFrame::Particle{
+											 findLeft
+											 , glm::vec3()
+											 , 1
+											});
+									}
 								}
 							}
 						}
